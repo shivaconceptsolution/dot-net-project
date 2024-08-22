bdfdb62a2a8055591636905539bc967fde0ddd82
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-08-22 11:06:36 +0530

    Add files via upload

---
+++ ProductcustomController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Productreview.Models;
+
+namespace Productreview.Controllers
+{
+    public class ProductcustomController : Controller
+    {
+        private  AppDbContext db;
+
+        public ProductcustomController(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            db.Products.Add(p);
+            db.SaveChanges();
+            ViewBag.data = "Product Added Successfully";
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+    }
+}
